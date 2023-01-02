111cd46a5fc0b2a859c35b98a6f46ed96dfef877
fumtas1k <fumta01a7@gmail.com>
2023-01-02 12:30:31 +0900

    [add]day07の演習1追加

---
+++ day07/exercise01.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+# 1-1
+echo "シェルスクリプトを勉強しています"
+
+# 1-2
+mkdir -p day07/mydir/subdir
+
+# 1-3
+cd day07/mydir/subdir
+cat <> blank.txt
+cat <> test.txt
+
+# 1-4
+ls | xargs chmod 755
+echo "# subdir内のファイル"
+ls -l
+
+# 1-5
+cd ..
+echo "# mydir内のファイル"
+ls -l
+
+# 1-6
+for i in {1..5}; do
+  echo "abcde" >> hello.txt
+done
+
+# 1-7
+cp -p hello.txt hello.txt.copy
+
+# 1-8
+rm hello.txt
+
+# 1-9
+echo "# hello.txt.copyの中身"
+cat hello.txt.copy
+
+# 1-10
+echo "# 現在のディレクトリ"
+realpath $(pwd)
